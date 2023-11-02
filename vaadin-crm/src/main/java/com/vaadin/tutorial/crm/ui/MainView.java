eab5fbf578388701a8cbc2745f14bea5de5a62f3
josejavier1059 <72498118+josejavier1059@users.noreply.github.com>
2023-11-02 15:36:54 +0100

    Entorno
    Entorno listo para trabajar en el

---
+++ vaadin-crm/src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -0,0 +1,18 @@
+package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+
+    public MainView() {
+
+
+
+
+    }
+
+
+}
---
+++ vaadin-crm/src/test/java/com/vaadin/tutorial/crm/MainViewIT.java
@@ -0,0 +1,69 @@
+package com.vaadin.tutorial.crm;
+
+import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.flow.component.button.testbench.ButtonElement;
+import com.vaadin.flow.component.notification.testbench.NotificationElement;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+public class MainViewIT extends AbstractViewTest {
+
+    @Test
+    public void clickingButtonShowsNotification() {
+        Assert.assertFalse($(NotificationElement.class).exists());
+        $(ButtonElement.class).waitForFirst().click();
+        Assert.assertTrue($(NotificationElement.class).waitForFirst().isOpen());
+    }
+
+    @Test
+    public void clickingButtonTwiceShowsTwoNotifications() {
+        Assert.assertFalse($(NotificationElement.class).exists());
+        ButtonElement button = $(ButtonElement.class).waitForFirst();
+        button.click();
+        button.click();
+        $(NotificationElement.class).waitForFirst();
+        Assert.assertEquals(2, $(NotificationElement.class).all().size());
+    }
+
+    @Test
+    public void buttonIsUsingLumoTheme() {
+        WebElement element = $(ButtonElement.class).waitForFirst();
+        assertThemePresentOnElement(element, Lumo.class);
+    }
+
+    @Test
+    public void testClickButtonShowsHelloAnonymousUserNotificationWhenUserNameIsEmpty() {
+        ButtonElement button = $(ButtonElement.class).waitForFirst();
+        button.click();
+        $(NotificationElement.class).waitForFirst();
+        Assert.assertTrue($(NotificationElement.class).exists());
+        NotificationElement notification = $(NotificationElement.class).first();
+        Assert.assertEquals("Hello anonymous user", notification.getText());
+    }
+
+    @Test
+    public void testClickButtonShowsHelloUserNotificationWhenUserIsNotEmpty() {
+        TextFieldElement textField = $(TextFieldElement.class).waitForFirst();
+        textField.setValue("Vaadiner");
+        $(ButtonElement.class).waitForFirst().click();
+        $(NotificationElement.class).waitForFirst();
+        Assert.assertTrue($(NotificationElement.class).exists());
+        NotificationElement notification = $(NotificationElement.class).first();
+        Assert.assertEquals("Hello Vaadiner", notification.getText());
+    }
+
+    @Test
+    public void testEnterShowsHelloUserNotificationWhenUserIsNotEmpty() {
+        TextFieldElement textField = $(TextFieldElement.class).waitForFirst();
+        textField.setValue("Vaadiner");
+        textField.sendKeys(Keys.ENTER);
+        $(NotificationElement.class).waitForFirst();
+        Assert.assertTrue($(NotificationElement.class).exists());
+        NotificationElement notification = $(NotificationElement.class).first();
+        Assert.assertEquals("Hello Vaadiner", notification.getText());
+    }
+}
