ce751e48891beaeefe67d966b293f417d2ad1067
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-01-04 00:53:55 +0100

    Añadido icono

--- easyCall/src/main/java/com/easycall/project/Application.java
+++ easyCall/src/main/java/com/easycall/project/Application.java
@@ -1,10 +1,12 @@
 package com.easycall.project;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@PWA(name = "EasyCall", shortName = "eCall", startPath = "your-start-path", iconPath = "src/main/resources/META-INF/resources/icons/icon.png")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
